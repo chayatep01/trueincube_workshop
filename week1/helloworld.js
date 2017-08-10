c6d666273d64675e2505f5cfcf1bf42d8941c585
chayatep <chayatep@chayateps-MacBook-Pro.local>
2017-08-10 16:28:07 +0700

    afternoon lesson

--- week1/helloworld.js
+++ week1/helloworld.js
@@ -5,9 +5,21 @@ var http = require('http');
 //try to use external lib
 var uppercase = require('upper-case');
 var abc = require('./course');
+//try urlparse**
+var url = require('url');
+var fs = require('fs');
+
+
 var server = http.createServer(function (req,res) {
+  //**
+  fs.readFile('./index.html',function(err,content){
+
+    res.end(content);
+  });
+
+  //res.end("hello " + url.parse(req.url ,true).query.name)
   //res.end(uppercase("Hello world"));
-  res.end(uppercase(abc.myCourse()));
+    //res.end(uppercase(abc.myCourse()));
 });
 //create sever
 server.listen(3030,function () {
